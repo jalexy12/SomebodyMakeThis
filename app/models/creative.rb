cd6ea2c546b2e4d4ebc13cc1f669f7f8324c9bf7
josh <jalexy12@gmail.com>
2015-07-05 00:09:13 -0400

    Added relationships between creative/developer and comments

--- app/models/creative.rb
+++ app/models/creative.rb
@@ -3,6 +3,6 @@ class Creative < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+         has_many :comments
          acts_as_voter
 end
--- app/models/developer.rb
+++ app/models/developer.rb
@@ -3,7 +3,7 @@ class Developer < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+         has_many :comments
          acts_as_voter 
 
 end
